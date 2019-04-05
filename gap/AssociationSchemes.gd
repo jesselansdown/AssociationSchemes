4c8a6d5a1fbc6bef87693dbadc2a9c364ca4a72c
Jesse Lansdown <jesselansdown@gmail.com>
2019-04-05 10:09:04 +0800

    Added comment to documentation that non-standard relations may be given.

--- gap/AssociationSchemes.gd
+++ gap/AssociationSchemes.gd
@@ -36,7 +36,8 @@ BindGlobal( "TheTypeHomogeneousCoherentConfiguration",
 #! @Description
 #!	Takes the relationship matrix, $M$, describing a coherent configuration and returns a CoherentConfiguration object.
 #!	The matrix $M = \sum_{i=0}^d i A_i$, where $A_i$ are the adjacency matrices describing a coherent configuration.
-#!	Checks that the matrix satisfies the coherent configuration axioms.
+#!	Checks that the matrix satisfies the coherent configuration axioms. (Note that this accepts a matrix of the form
+#!	$M = \sum_{i=0}^d a_i A_i$ where $a_i$ is not equal to $i$, however, it will first convert to the form $M = \sum_{i=0}^d i A_i$).
 DeclareOperation( "HomogeneousCoherentConfiguration", [ IsMatrix ] );
 
 #! @Chapter Functionality
@@ -55,7 +56,8 @@ DeclareOperation( "HomogeneousCoherentConfigurationNC", [ IsMatrix ] );
 #! @Description
 #!	Takes the relationship matrix, $M$, describing an associatioin scheme and returns a association scheme (symmetric coherent configuration).
 #!	The matrix $M = \sum_{i=0}^d i A_i$, where $A_i$ are the adjacency matrices describing an association scheme.
-#!	Checks that the matrix satisfies the association scheme axioms.
+#!	Checks that the matrix satisfies the association scheme axioms.(Note that this accepts a matrix of the form
+#!	$M = \sum_{i=0}^d a_i A_i$ where $a_i$ is not equal to $i$, however, it will first convert to the form $M = \sum_{i=0}^d i A_i$).
 DeclareOperation( "AssociationScheme", [ IsMatrix ] );
 
 #! @Chapter Functionality
